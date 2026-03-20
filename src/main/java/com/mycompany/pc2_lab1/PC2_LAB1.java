5477226077a75dffeb1bef7d2790c98767594216
Alumno <Alumno@PC23-LAB2>
2026-03-19 21:31:10 -0500

    Commit_1

---
+++ src/main/java/com/mycompany/pc2_lab1/PC2_LAB1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pc2_lab1;
+
+/**
+ *
+ * @author Alumno
+ */
+public class PC2_LAB1 {
+
+    public static void main(String[] args) {
+        System.out.println("");
+            
+    }
+}
